51d3c610914ff10fe4208f457f9c43b591834b53
DimkoPY <nikolaevdm1985@gmail.com>
2021-03-25 14:51:22 +0300

    filtering by category

--- listings/urls.py
+++ listings/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'listings'
 
 urlpatterns = [
-    path('', views.product_list, name='product_list')
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>', views.product_list, name='product_list_by_category')
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
 # Create your views here.
 
-def product_list(request):
+def product_list(request, category_slug=None):
     categories = Category.objects.all()
-    products = Product.objects.all()
+
+    if category_slug:
+        requested_category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=requested_category)
+    else:
+        requested_category = None
+        products = Product.objects.all()
 
     return render(
         request,
         'product/list.html',
         {
             'categories': categories,
+            'requested_category': requested_category,
             'products': products
         }
     )
